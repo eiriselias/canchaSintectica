b34ccd8dfe27b88419bd50463a38cb3820187267
eiriselias <eiriselias@gmail.com>
2022-09-06 14:05:05 -0500

    se creo la tabla de posiciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PosicionesComponent } from './components/posiciones/posiciones.component';
 import { ProgramacionComponent } from './components/programacion/programacion.component';
 
 const routes: Routes = [
  
-  {path:"programacion",component:ProgramacionComponent}
+  {path:"programacion",component:ProgramacionComponent},
+  {path:"posiciones",component:PosicionesComponent}
 ];
 
 @NgModule({
