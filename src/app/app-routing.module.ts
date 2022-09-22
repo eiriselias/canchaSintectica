a9ecdbfa33138c7a8391b114c40172dd485d1979
eiriselias <eiriselias@gmail.com>
2022-09-22 09:33:53 -0500

    cambio de link imagenes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { PosicionesComponent } from './components/posiciones/posiciones.componen
 import { ProgramacionComponent } from './components/programacion/programacion.component';
 
 const routes: Routes = [
-  {path:"", component:ProgramacionComponent},
+  {path:"", component:ProgramacionComponent, pathMatch:'full'},
   {path:"programacion",component:ProgramacionComponent},
   {path:"posiciones",component:PosicionesComponent},
   {path:"goleadores", component:GoleadoresComponent},
   {path:"disponibilidad", component:DisponibilidadComponent},
-  {path:"crearEquipo", component:CrearEquipoComponent}
+  {path:"crearEquipo", component:CrearEquipoComponent},
+  {path:"**", redirectTo:'/', pathMatch:'full'}
 ];
 
 @NgModule({
